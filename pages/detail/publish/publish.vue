3d8ad1fadd070d0d5465ca76bb6ed8c25bc80e8d
koori1238 <huanbingwang830@gmail.com>
2025-04-15 11:53:17 +0900

    提交文件

---
+++ pages/detail/publish/publish.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="container">
+    <text>This is publish page</text>
+  </view>
+</template>
+
+<script>
+export default {
+  onLoad() {
+    console.log("enter publish page");
+  }
+}
+</script>
+
+<style>
+.container {
+  padding: 50rpx;
+}
+</style>
